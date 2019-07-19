768ff36f69e0cea0ca997e06e55976d5afdd6c18
League Student <league@iMac-24.attlocal.net>
2019-07-18 18:29:08 -0700

    Working on checkpoint

---
+++ src/Checkpoint/TextEditor.java
@@ -0,0 +1,22 @@
+package Checkpoint;
+
+public class TextEditor {
+
+	JFrame frame;
+	JPanel panel;
+	JButton save;
+	JButton load;
+	JTextArea text;
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	
+	
+}
